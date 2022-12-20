b3a788112ea71a3bdcce1857bb930c672abcbc53
Aryan Ahire <aryanahire9086360780@gmail.com>
2022-12-20 16:40:46 -0500

    made spacewar background darker

--- themes/doom-spacewar-theme.el
+++ themes/doom-spacewar-theme.el
@@ -42,23 +42,23 @@ Can be an integer to determine the exact padding."
   "A dark theme based off of doom-one with more spacewar colors."
 
   ;; name        gui       256           16
-  ((bg         '("#242225" "black"       "black" ))
+  ((bg         '("#141215" "black"       "black" ))
    (fg         '("#CFC8C2" "#bfbfbf"     "brightwhite" ))
 
    ;; These are off-color variants of bg/fg, used primarily for `solaire-mode',
    ;; but can also be useful as a basis for subtle highlights (e.g. for hl-line
    ;; or region), especially when paired with the `doom-darken', `doom-lighten',
    ;; and `doom-blend' helper functions.
-   (bg-alt     '("#353337" "black"       "black"       ))
+   (bg-alt     '("#252327" "black"       "black"       ))
    (fg-alt     '("#BEB7B2" "#5d5d5d"     "white"       ))
 
    ;; These should represent a spectrum from bg to fg, where base0 is a starker
    ;; bg and base8 is a starker fg. For example, if bg is light grey and fg is
    ;; dark grey, base0 should be white and base8 should be black.
    (base0      '("#131214" "#101010"     "black"       ))
-   (base1      '("#242225" "#1e1e1e"     "brightblack" ))
-   (base2      '("#353337" "#21212d"     "brightblack" ))
-   (base3      '("#46434A" "#262626"     "brightblack" ))
+   (base1      '("#141215" "#1e1e1e"     "brightblack" ))
+   (base2      '("#252327" "#21212d"     "brightblack" ))
+   (base3      '("#36333A" "#262626"     "brightblack" ))
    (base4      '("#8B8482" "#5e5e5e"     "brightblack" ))
    (base5      '("#9C9592" "#666666"     "brightblack" ))
    (base6      '("#ADA6A2" "#7b7b7b"     "brightblack" ))
