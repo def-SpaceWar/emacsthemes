1555e1460cae4a9edff652448ba1efa6d1864fe7
Aryan Ahire <aryanahire9086360780@gmail.com>
2023-01-04 22:25:20 -0500

    Modified one-monokai with less vibrant colors

--- themes/doom-one-monokai-theme.el
+++ themes/doom-one-monokai-theme.el
@@ -39,44 +39,44 @@ Can be an integer to determine the exact padding."
 ;;; Theme definition
 
 (def-doom-theme doom-one-monokai
-  "A dark theme based off of doom-one with more vibrant colors."
+  "A dark theme based off of doom-one with monokai's color pattern."
 
-  ;; name        gui       256           16
-  ((bg         '("#242730" "black"       "black" ))
-   (fg         '("#bbc2cf" "#bfbfbf"     "brightwhite" ))
+  ;; name        default   256           16
+  ((bg         '("#282c34" "black"       "black"  ))
+   (fg         '("#bbc2cf" "#bfbfbf"     "brightwhite"  ))
 
    ;; These are off-color variants of bg/fg, used primarily for `solaire-mode',
    ;; but can also be useful as a basis for subtle highlights (e.g. for hl-line
    ;; or region), especially when paired with the `doom-darken', `doom-lighten',
    ;; and `doom-blend' helper functions.
-   (bg-alt     '("#2a2e38" "black"       "black"       ))
-   (fg-alt     '("#5D656B" "#5d5d5d"     "white"       ))
+   (bg-alt     '("#21242b" "black"       "black"        ))
+   (fg-alt     '("#5B6268" "#2d2d2d"     "white"        ))
 
    ;; These should represent a spectrum from bg to fg, where base0 is a starker
    ;; bg and base8 is a starker fg. For example, if bg is light grey and fg is
    ;; dark grey, base0 should be white and base8 should be black.
-   (base0      '("#1c1f24" "#101010"     "black"       ))
-   (base1      '("#1c1f24" "#1e1e1e"     "brightblack" ))
-   (base2      '("#21272d" "#21212d"     "brightblack" ))
-   (base3      '("#23272e" "#262626"     "brightblack" ))
-   (base4      '("#484854" "#5e5e5e"     "brightblack" ))
-   (base5      '("#62686E" "#666666"     "brightblack" ))
-   (base6      '("#757B80" "#7b7b7b"     "brightblack" ))
-   (base7      '("#9ca0a4" "#979797"     "brightblack" ))
-   (base8      '("#DFDFDF" "#dfdfdf"     "white"       ))
+   (base0      '("#1B2229" "black"       "black"        ))
+   (base1      '("#1c1f24" "#1e1e1e"     "brightblack"  ))
+   (base2      '("#202328" "#2e2e2e"     "brightblack"  ))
+   (base3      '("#23272e" "#262626"     "brightblack"  ))
+   (base4      '("#3f444a" "#3f3f3f"     "brightblack"  ))
+   (base5      '("#5B6268" "#525252"     "brightblack"  ))
+   (base6      '("#73797e" "#6b6b6b"     "brightblack"  ))
+   (base7      '("#9ca0a4" "#979797"     "brightblack"  ))
+   (base8      '("#DFDFDF" "#dfdfdf"     "white"        ))
 
    (grey       base4)
-   (red        '("#ff665c" "#ff6655" "red"             ))
-   (orange     '("#e69055" "#dd8844" "brightred"       ))
-   (green      '("#7bc275" "#99bb66" "green"           ))
-   (teal       '("#4db5bd" "#44b9b1" "brightgreen"     ))
-   (yellow     '("#FCCE7B" "#ECBE7B" "yellow"          ))
-   (blue       '("#51afef" "#51afef" "brightblue"      ))
-   (dark-blue  '("#1f5582" "#2257A0" "blue"            ))
-   (magenta    '("#C57BDB" "#c678dd" "brightmagenta"   ))
-   (violet     '("#a991f1" "#a9a1e1" "magenta"         )) ;a9a1e1
-   (cyan       '("#5cEfFF" "#46D9FF" "brightcyan"      ))
-   (dark-cyan  '("#6A8FBF" "#5699AF" "cyan"            ))
+   (red        '("#ff6c6b" "#ff6655" "red"          ))
+   (orange     '("#da8548" "#dd8844" "brightred"    ))
+   (green      '("#98be65" "#99bb66" "green"        ))
+   (teal       '("#4db5bd" "#44b9b1" "brightgreen"  ))
+   (yellow     '("#ECBE7B" "#ECBE7B" "yellow"       ))
+   (blue       '("#51afef" "#51afef" "brightblue"   ))
+   (dark-blue  '("#2257A0" "#2257A0" "blue"         ))
+   (magenta    '("#c678dd" "#c678dd" "brightmagenta"))
+   (violet     '("#a9a1e1" "#a9a1e1" "magenta"      ))
+   (cyan       '("#46D9FF" "#46D9FF" "brightcyan"   ))
+   (dark-cyan  '("#5699AF" "#5699AF" "cyan"         ))
 
    ;; These are the "universal syntax classes" that doom-themes establishes.
    ;; These *must* be included in every doom themes, or your theme will throw an
